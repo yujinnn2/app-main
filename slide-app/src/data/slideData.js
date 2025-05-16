cbc1b9fe557a061b4fbc90e7ec31d8d4169c79bf
yujinnn2 <yujinee111@gmail.com>
2025-05-16 19:36:15 +0900

    dd

--- slide-app/src/data/slideData.js
+++ slide-app/src/data/slideData.js
@@ -2,7 +2,7 @@ const slideDate= [
     {
         title:'인기상품',
         items:[
-            {title:'현수 무료나눔' , image: './images/a.jfif'},
+            {title:'현수 무료나눔' , image: './assets/imgaes/a.'},
             {title:'수아 ?원' , image: './images/b.jfif'},
             {title:'영원 0원' , image: './images/c.jfif'},
         ]
